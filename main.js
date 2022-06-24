471a920514c92ece1bb4099da52a6bd89e1d2d50
Khinshuk <91619702+Khinshuk@users.noreply.github.com>
2022-06-24 19:38:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+noseX="";
+noseY="";
+
+function preload(){
+
+}
+
+function setup(){
+canvas= createCanvas(400,400);
+canvas.center();
+video= createCapture(VIDEO);
+video.size(400,400);
+video.hide()
+
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+
+function draw(){
+image(video,0,0,400,400)
+}
+
+function take_snapshot(){
+    save('pic.png');
+}
+
+function gotPoses(results){
+    if(results.length>0){
+    noseX=results[0].pose.nose.x;
+    noseY=results[0].pose.nose.y;
+    console.log('noseX= '+results[0].pose.nose.x);
+    console.log('noseY= '+results[0.].pose.nose.y);
+    }
+}
+
+function modelLoaded(){
+    console.log('poseNet is initialized');
+}
